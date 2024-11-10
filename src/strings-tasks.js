13ccb43e95443206ad2537daaf4a5a854a1ee515
Sergei Shinkarenko <exp33int@gmail.com>
2024-11-10 22:40:46 +0500

    feat: implement countOfVowels func

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -308,8 +308,8 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  return str.replace(/[^aeiouy]/gi, '').length;
 }
 
 /**
